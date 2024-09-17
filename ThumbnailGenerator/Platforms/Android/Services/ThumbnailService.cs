de5ea67ba026e324dd3f24611cf85eafe89b25c5
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-09-17 10:28:26 +0100

    Implemented Android
    This was alot nicer to do on android actually

--- ThumbnailGenerator/Platforms/Android/Services/ThumbnailService.cs
+++ ThumbnailGenerator/Platforms/Android/Services/ThumbnailService.cs
@@ -1,3 +1,7 @@
+using Android.Graphics;
+using Android.Media;
+using Android.Provider;
+
 namespace ThumbnailGenerator.Services;
 
 public partial class ThumbnailService
@@ -6,7 +10,23 @@ public partial class ThumbnailService
     {
         try
         {
-            throw new NotImplementedException();
+            var bitmap = await ThumbnailUtils.CreateVideoThumbnailAsync(filePath, ThumbnailKind.MiniKind);
+
+            if (bitmap is null)
+            {
+                throw new FileNotFoundException($"Could not load thumbnail from file path: {filePath}");
+            }
+            
+            using var stream = new MemoryStream();
+            var success = await bitmap.CompressAsync(Bitmap.CompressFormat.Jpeg, 75, stream);
+
+            if (!success)
+            {
+                throw new InvalidOperationException("Could not turn thumbnail bitmap into jpeg");
+            }
+
+            var generatedThumbnail = new ThumbnailGenerated(stream.ToArray());
+            return new GenerateThumbnailResult(generatedThumbnail);
         }
         catch (Exception ex)
         {
